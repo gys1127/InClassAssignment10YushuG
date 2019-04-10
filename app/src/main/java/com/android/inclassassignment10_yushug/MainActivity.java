63600a21b6023ea649151ab517abf19ffb618e0f
yushuguo <yg2590@tc.columbia.edu>
2019-04-09 21:03:55 -0400

    Initial commit

---
+++ app/src/main/java/com/android/inclassassignment10_yushug/MainActivity.java
@@ -0,0 +1,51 @@
+package com.android.inclassassignment10_yushug;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private DatabaseReference userRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        userInput = (EditText) findViewById(R.id.user_input);
+
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user == null) {
+                    startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                } else {
+                    userRef = database.getReference(user.getUid());
+                }
+            }
+        };
+        auth.addAuthStateListener(authStateListener);
+    }
+
+    public void send(View view) {
+        userRef.setValue(userInput.getText().toString());
+    }
+}
